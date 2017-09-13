9e45306d2d090c8022f2f06ad989162005b4194b
omnomnom08 <dasha96schipanova@mail.ru>
2017-09-14 00:45:28 +0300

    Rewrite

---
+++ LeetCode557/LeetCode557/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode557
+{
+    public class Solution
+    {
+        public static string ReverseWords(string s)
+        {
+            string unS;
+            int k = -1;
+            char [] str = s.ToCharArray();
+            for (int i = 0; i < s.Length; i++)
+            {
+                int NewIndex = s.IndexOf(" ", i) + k - i;
+              //  Console.WriteLine("PROBEL");
+              //  Console.WriteLine(s.IndexOf(" ", i));
+                if (NewIndex<-1)
+                {
+                    NewIndex =  s.Length - i + s.LastIndexOf(" ");                  
+                };
+                if (i == s.IndexOf(" ", i))
+                {
+                    NewIndex = i;
+                    k = i;
+                }
+             //   Console.WriteLine("NEWINDEX");
+             //   Console.WriteLine(NewIndex);
+                s.CopyTo(i, str, NewIndex, 1);
+                
+            };
+            unS = String.Concat<char>(str);
+         
+            return (unS);
+        }
+    }
+       
+    class Program
+    {
+         static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            string t = LeetCode557.Solution.ReverseWords(s);
+            Console.WriteLine(t);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ LeetCode633/LeetCode633/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode633
+{
+    public class Solution
+    {
+        public static bool JudgeSquareSum(int c)
+          {
+            int a = -1;
+            double h = 1;
+            int intb = 0;
+            double b = 0;
+
+            do
+            {
+                a++;
+                b = Math.Sqrt((double)(c - a * a));
+                intb = (int)(b);
+                h = b - intb;
+            }
+            while (a * a <= c / 2 && h != 0);
+
+
+            if ((a * a + intb * intb) == c)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+
+        }
+        static void Main(string[] args) 
+        {
+            int c = Convert.ToInt32(Console.ReadLine());
+
+            if (LeetCode633.Solution.JudgeSquareSum(c) == true) 
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
+            Console.ReadKey(); 
+        }
+    }
+}
